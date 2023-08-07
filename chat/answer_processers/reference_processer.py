fed9616850c66d629be4eda58125d01ff50afc96
Royywangg233 <133733744+Royywangg233@users.noreply.github.com>
2023-08-07 18:35:36 +0800

    Update reference_processer.py

--- chat/answer_processers/reference_processer.py
+++ chat/answer_processers/reference_processer.py
@@ -10,15 +10,24 @@ def get_reference_list(SOURCE):
 def replace_with_http_link(match):
     base_url = "http://localhost:3000/#/"
     path = match.group(0)
-    # 移除 "/TingyunAI/yuque/" 部分
-    path = path.replace("/Users/wanghao/Desktop/docs/", "")
+    
+    # 找到最后一个斜杠的索引
+    last_slash_index = load_file_path.rfind('/')
+    
+    # 提取包含最后一个斜杠之前的部分
+    extracted_part = load_file_path[:last_slash_index + 1]
+    path = path.replace(extracted_part, "")
+    
     # 移除 ".md" 部分
     path = path.replace(".md", "")
+    
     # 使用 urllib.parse.quote 对路径进行 URL 编码
     encoded_path = urllib.parse.quote(path)
+    
     return f'<a href="{base_url}{encoded_path}">{path}.md</a>'
 
 
 def reference_pattern():
-    pattern = r"/Users/wanghao/Desktop/docs/bangzhu/.*\.md"
+    #pattern = r'/Users/wanghao/Desktop/docs/[\w\.\-\[\]【】\u4e00-\u9fa5&、《》，：]+/[\w\.\,\=\'\-\[\]【】\u4e00-\u9fa5\(\)\+\s\uff08\uff09\u3002\u300a\u300b&【】·、？?{}\uff08\uff09，：\u201c\u201d%——《》…]+\.md'
+    pattern = fr"{load_file_path}/.*\.md"
     return pattern
